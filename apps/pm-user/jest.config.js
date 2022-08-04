ab397745063794fe28434272963110cfd0e0d7cf
Karthik <jsdevkarthik@gmail.com>
2022-08-04 12:23:01 +0530

    Added React Support in Nx Workspace and created 'pm-user' React Application

---
+++ apps/pm-user/jest.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'pm-user',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/pm-user',
+};
