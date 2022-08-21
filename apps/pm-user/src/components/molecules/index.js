ca190b21b4e45d78a9a87ac2538a7bd678b2d4ab
Karthik <jsdevkarthik@gmail.com>
2022-08-21 16:34:24 +0530

    Added BigRecommendationCard component with notification feature.

--- apps/pm-user/src/components/molecules/index.js
+++ apps/pm-user/src/components/molecules/index.js
@@ -1,6 +1,7 @@
-export { default as NavBar } from './navBar';
+export { default as BigRecommendationCard } from './bigRecommendationCard';
 export { default as LoginForm } from './loginForm';
 export { default as MyCard } from './myCard';
+export { default as NavBar } from './navBar';
 export { default as OwnProfileSummaryCard } from './ownProfileSummaryCard';
 export { default as ProfileSummary } from './profileSummary';
 export { default as QuickInfoBar } from './quickInfoBar';
