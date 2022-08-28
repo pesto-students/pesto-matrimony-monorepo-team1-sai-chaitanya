14bb2a8f9fd28087c8ce9faa5c0b4fb5ddf16b7b
Karthik <jsdevkarthik@gmail.com>
2022-08-28 19:52:02 +0530

    Deleted DeclinedInfoButtons component. It is no longer needed.

--- apps/pm-user/src/components/molecules/index.js
+++ apps/pm-user/src/components/molecules/index.js
@@ -1,25 +1,25 @@
-export { default as AcceptDeclineButtons } from './acceptDeclineButtons';
-export { default as DeclinedInfoButtons } from './declinedInfoButtons';
-export { default as InterestBoxButtons } from './interestBoxButtons';
+// export { default as AcceptDeclineButtons } from './acceptDeclineButtons';
+// export { default as DeclinedInfoButtons } from './declinedInfoButtons';
+// export { default as InterestBoxButtons } from './interestBoxButtons';
 export { default as LoginForm } from './loginForm';
 export { default as MessagesBox } from './messagesBox';
 export { default as MyCard } from './myCard';
 export { default as NavBar } from './navBar';
-export { default as OldMessages } from './oldMessages'
+export { default as OldMessages } from './oldMessages';
 export { default as OwnProfileSummaryCard } from './ownProfileSummaryCard';
 export { default as ProfileSummary } from './profileSummary';
 export { default as QuickInfoBar } from './quickInfoBar';
 export { default as ResultSection } from './resultSection';
-export { default as SearchProfiles } from './searchProfiles'
+export { default as SearchProfiles } from './searchProfiles';
 export { default as UserItemsList } from './userItemsList';
 export { default as UserInfoCard } from './userInfoCard';
 export { default as UserInfoCardsList } from './userInfoCardsList';
 export { default as ViewAndSendButtons } from './viewAndSendButtons';
 
-export { default as SendInterestButton } from './sendInterestButton'
-export { default as ShortlistButton } from './shortlistButton'
-export { default as SendMessageButton } from './sendMessageButton'
-export { default as CancelInterestButton } from './cancelInterestButton'
-export { default as AcceptInterestButton } from './acceptInterestButton'
-export { default as RejectInterestButton } from './rejectInterestButton'
-export { default as UserInfoCardButtons } from './userInfoCardButtons'
+export { default as SendInterestButton } from './sendInterestButton';
+export { default as ShortlistButton } from './shortlistButton';
+export { default as SendMessageButton } from './sendMessageButton';
+export { default as CancelInterestButton } from './cancelInterestButton';
+export { default as AcceptInterestButton } from './acceptInterestButton';
+export { default as RejectInterestButton } from './rejectInterestButton';
+export { default as UserInfoCardButtons } from './userInfoCardButtons';
