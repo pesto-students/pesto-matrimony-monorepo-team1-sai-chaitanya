0e2da30a312f542a899321345c6da65538082bf5
Karthik <jsdevkarthik@gmail.com>
2022-09-08 23:24:31 +0530

    prettified code.

--- apps/pm-backend/src/routes/admin.js
+++ apps/pm-backend/src/routes/admin.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
 //importing controllers
-const {
-  getAllUsersProfiles
-} = require('../controllers/admin');
+const { getAllUsersProfiles } = require('../controllers/admin');
 
 const router = express.Router();
 
 //getting all users of pesto-matrimony
-router.route('/getallusers').get(getAllUsersProfiles)
-
+router.route('/getallusers').get(getAllUsersProfiles);
 
 module.exports = router;
