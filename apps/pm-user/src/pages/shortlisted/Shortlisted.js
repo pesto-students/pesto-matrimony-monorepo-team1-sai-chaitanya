3288fe07ca4eb90e91d75247a8bf59a8cc53191c
Karthik <jsdevkarthik@gmail.com>
2022-09-10 21:25:05 +0530

    Completed Shortlisted Page.

--- apps/pm-user/src/pages/shortlisted/Shortlisted.js
+++ apps/pm-user/src/pages/shortlisted/Shortlisted.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 const Shortlisted = () => {
   const { authState } = useOktaAuth();
-  const [shortlistedProfileIds, setShortlistedProfileIds] = useState([]);
   const [shortlistedMatchesData, setShortlistedMatchesData] = useState([]);
 
   //getting current user's oktaId
@@ -17,44 +16,28 @@ const Shortlisted = () => {
       .get(`http://localhost:8000/api/v1/users/userprofile/${oktaUserId}`)
       .then((res) => {
         const idsOfShortlistedUsers = res.data.currentUser[0].shortlistedMatches;
-        console.log(idsOfShortlistedUsers);
         idsOfShortlistedUsers.forEach((id) => {
           axios
             .get(`http://localhost:8000/api/v1/users/userprofile/${id}`)
             .then((res) => {
-              const data = res.data.currentUser[0];
-              actualShortlistedMatcheshData.push(data);
+              actualShortlistedMatcheshData.push(res.data.currentUser[0]);
             })
             .catch((err) => console.log(err));
-            
         });
-        console.log(actualShortlistedMatcheshData);
+      })
+      .then(() => {
+        console.log('finally... ', actualShortlistedMatcheshData);
         setShortlistedMatchesData(actualShortlistedMatcheshData);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  //   if (shortlistedProfileIds.length > 0) {
-  //     shortlistedProfileIds.forEach((id) => {
-  //       axios
-  //         .get(`http://localhost:8000/api/v1/users/userprofile/${id}`)
-  //         .then((res) => {
-  //           actualShortlistedMatcheshData.concat(res.data.currentUser[0]);
-  //         })
-  //         .catch((err) => console.log(err));
-  //     });
-  //   }
-
   console.log(shortlistedMatchesData);
   return (
-    <>
-      {shortlistedMatchesData.length < 1 ? (
-        <p>You have not shortlisted any one. You can search and shortlist matches.</p>
-      ) : (
-        <UserInfoCardsList matchesData={shortlistedMatchesData} />
-      )}
-      ;
-    </>
+    <div>
+      <h2>Shortlisted Profiles</h2>
+      <UserInfoCardsList matchesData={shortlistedMatchesData} />
+    </div>
   );
 };
 
